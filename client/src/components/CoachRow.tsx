ca22bd33636ad33bf3481aa36b3397b1174586a3
unknown <git@github.com:VicThorMetaNode>
2023-09-01 02:18:30 +0200

    client side apollo cache done + TypeScript debugging

--- client/src/components/CoachRow.tsx
+++ client/src/components/CoachRow.tsx
@@ -1,5 +1,6 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, MutationUpdaterFn } from "@apollo/client";
 import { DELETE_COACH } from "../mutations/coachMutations";
+import { GET_COACHES } from "../queries/coachQueries";
 
 interface Coach {
   id: string;
@@ -12,18 +13,45 @@ interface CoachRowProps {
   coach: Coach;
 }
 
-const CoachRow: React.FC<CoachRowProps> = ({ coach }) => {
-  const [deleteCoach] = useMutation(DELETE_COACH, {
-    variables: {id: coach.id},
-    });
+interface DeleteCoachData {
+  deleteCoach: {
+    id: string;
+  };
+}
 
-    const handleClick = () => {
-      // Invoke the deleteCoach mutation function here
-      deleteCoach().catch((error) => {
-        console.error('Error deleting coach:', error);
+const CoachRow: React.FC<CoachRowProps> = ({ coach }) => {
+  const handleUpdateCache: MutationUpdaterFn<DeleteCoachData> = (
+    cache,
+    { data }
+  ) => {
+    if (data) {
+      const deletedCoachId = data.deleteCoach.id;
+      const { coaches } = cache.readQuery<{ coaches: Coach[] }>({
+        query: GET_COACHES,
+      }) || { coaches: [] };
+      const updatedCoaches = coaches.filter(
+        (coach) => coach.id !== deletedCoachId
+      );
+      cache.writeQuery<{ coaches: Coach[] }>({
+        query: GET_COACHES,
+        data: { coaches: updatedCoaches },
       });
-    };
-    
+    }
+  };
+
+  const [deleteCoach] = useMutation<DeleteCoachData>(DELETE_COACH, {
+    variables: { id: coach.id },
+    // Update cache so no need to refresh to see result
+    update: handleUpdateCache,
+  });
+
+  const handleClick = () => {
+    // Invoke the deleteCoach mutation function here
+    deleteCoach().catch((error) => {
+      console.error("Error deleting coach:", error);
+    });
+  };
+
   return (
     <tr className="md:hover transition ease-in-out duration-400">
       <td>{coach.name} </td>
@@ -31,7 +59,9 @@ const CoachRow: React.FC<CoachRowProps> = ({ coach }) => {
       <td>{coach.email} </td>
       <td>{coach.phone} </td>
       <td>
-        <button className="btn" onClick={handleClick}>delete</button>
+        <button className="btn" onClick={handleClick}>
+          delete
+        </button>
       </td>
     </tr>
   );
